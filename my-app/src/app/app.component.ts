838130b207d7201ff2e94b278cb12e7668fcdfb5
Rus0d <6087659a>
2017-10-20 02:01:58 +0300

    .

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,27 +1,40 @@
-import { Component } from '@angular/core';
-import { Router} from '@angular/router';
+import { Component, OnDestroy} from '@angular/core';
+import { ActivatedRoute} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import { APP_BASE_HREF } from '@angular/common';
+import { Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app';
 
-  constructor(private router: Router){}
 
-  goToItem(){
+  private product: string;
+  private price: string;
 
-    this.router.navigate(
-      ['', {'product': 'asd'}],
-      {
-        queryParams:{
-          'product': 'asd',
-          'price': 111
-        }
+  private querySubscription: Subscription;
+  constructor(private route: ActivatedRoute, @Inject(APP_BASE_HREF) private baseHref:string){
+
+    this.querySubscription = route.queryParams.subscribe(
+      (queryParam: any) => {
+        this.product = queryParam['product'];
+        this.price = queryParam['price'];
+
+        console.log(this.product, this.price, this.baseHref);
       }
     );
   }
+  ngOnDestroy(){
+    this.querySubscription.unsubscribe();
+  }
 
+  changeURL() {
+    this.baseHref = '?product=phone&price=200';
+    console.log(this.product, this.price, this);
+
+  }
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
+
+// определение маршрутов
+const appRoutes: Routes =[
+  { path: '', component: AppComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: ''}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
